30584c433ce245d44d96abc0f1c30084e4bde275
AzyzXXIII <azizhcini001@gmail.com>
2025-01-27 15:25:34 +0100

    WORK DONE

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "../build")));
 
 //CONTROLLERS
 const booksController = require("./controllers/book_controller");
@@ -20,4 +21,8 @@ app.use("/api/books", booksController);
 // LISTEN
 app.listen(4000, () => {
   console.log("Server is running on port 4000");
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build/index.html"));
 });
